90b9de96d2ca15d5c1f9edfcdd36d687413ff207
David Jr <david.junior@unesp.br>
2023-05-22 16:50:09 -0300

    feat: base for the carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Carousel from "@/components/carousel";
+
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+		<main className="flex flex-col items-center justify-between">
+			<Carousel />
+		</main>
 	);
 }
---
+++ src/components/carousel/index.tsx
@@ -0,0 +1,7 @@
+export default function Carousel() {
+	return (
+		<div className="bg-cyan-500 w-full max-w-7xl h-96">
+			<h1 className="text-4xl"> Carousel</h1>
+		</div>
+	);
+}
