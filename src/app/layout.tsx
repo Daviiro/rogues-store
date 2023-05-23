17716c1af5cd29ef2e66836ea2ed7af3e16ddf9d
David Jr <david.junior@unesp.br>
2023-05-23 11:23:27 -0300

    feat: added the base for the footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,10 +16,16 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>
+		<html lang="en" className="h-full">
+			<body
+				className={
+					inter.className +
+					"w-full h-full grid grid-rows-[auto_1fr_auto] grid-cols-1"
+				}
+			>
 				<Header />
 				{children}
+				<Footer />
 			</body>
 		</html>
 	);
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<footer className="w-full h-24 bg-purple-600">
+			This gonna be the footer of the application
+		</footer>
+	);
+}
