411adf21bde62061afc2ad8fd0322594c312d6fb
David Jr <david.junior@unesp.br>
2023-05-26 17:55:52 -0300

    feat: started to style the footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,26 @@
+import Link from "next/link";
+
 export default function Footer() {
 	return (
-		<footer className="w-full h-24 bg-purple-600">
-			This gonna be the footer of the application
+		<footer className="w-full h-20 p-2 bg-white text-gray-600">
+			<div className="max-w-7xl m-auto">
+				<div className="flex justify-between text-sm">
+					<Link href="/">Trabalhe conosco</Link>
+
+					<Link href="/">Termos e condições</Link>
+
+					<Link href="/">Como cuidamos da sua privacidade</Link>
+					<Link href="/">Acessibilidade</Link>
+					<Link href="/">Contato</Link>
+					<Link href="/">Informações sobre seguros</Link>
+				</div>
+				<p className="text-gray-500 dark:text-gray-400 text-xs">
+					Copyright © 1999-2023 Rogues.com.br LTDA.
+				</p>
+				<p className="text-gray-500 dark:text-gray-400 text-xs">
+					CNPJ n.º 123456789-10
+				</p>
+			</div>
 		</footer>
 	);
 }
